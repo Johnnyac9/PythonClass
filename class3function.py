50f29329e17ba07578ca9a475ce029c5bbe16f33
Johnnyac9 <57197547+Johnnyac9@users.noreply.github.com>
2019-11-06 19:04:48 -0500

    Add files via upload
    Class 3

---
+++ class3function.py
@@ -0,0 +1,15 @@
+def helloworld():
+    print("hello World")
+
+def numberloop():
+    numbers = [1, 2, 3, 4, 5]
+    for x in numbers:
+        print(x)
+
+def numberloop1():
+    print("numberloop1")
+
+
+
+#    print(number loop 10):
+#       print(x + 10)
---
+++ class3mainfile.py
@@ -0,0 +1,16 @@
+import class3function
+
+
+def main():
+    class3function.helloworld()
+
+
+main()
+
+import class3function
+
+def main():
+    class3function.numberloop1()
+    class3function.numberloop()
+
+main()
